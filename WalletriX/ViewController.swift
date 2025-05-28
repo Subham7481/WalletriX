6c37ea204af5e0933066e6e600aba9650d66ae74
Subham Kaumar <patelsk7481@gmail.com>
2025-05-28 10:11:14 +0530

    Initial Commit

---
+++ WalletriX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalletriX
+//
+//  Created by Subham Patel on 28/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WalletriXTests/WalletriXTests.swift
@@ -0,0 +1,17 @@
+//
+//  WalletriXTests.swift
+//  WalletriXTests
+//
+//  Created by Subham Patel on 28/05/25.
+//
+
+import Testing
+@testable import WalletriX
+
+struct WalletriXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
